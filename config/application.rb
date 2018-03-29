6c1eb188376c819b5ad4df32e336dfb35e70f5f9
Andrew Morrison <morro141@gmail.com>
2018-03-29 18:40:53 -0400

    Few modifications

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,15 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+env_path = File.expand_path(File.join(__dir__,'_env.rb'))
+load env_path if File.exist?(env_path)
+
+# load EY Memached yml config (if exists)
+memcached_path = File.expand_path(File.join(__dir__, 'memcached.yml'))
+if File.exist?(memcached_path)
+  parsed = YAML.load(IO.read(memcached_path))
+  ENV['MEMCACHE_SERVERS'] = parsed['defaults']['servers'].join(',')
+end
 
 module BHuman
   class Application < Rails::Application
